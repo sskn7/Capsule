e3fb864805fc6443f894c99f9b1453025bb9efbb
kana sasazawa <kana.yellow7@gmail.com>
2025-03-20 22:11:24 +0900

    演習１から３

---
+++ src/jp/co/capsule/Capsule.java
@@ -0,0 +1,10 @@
+package jp.co.capsule;
+
+public class Capsule {
+	//演習２
+	public static void main (String[] args) {
+		Person taro = new Person ("山田太郎",20);
+		System.out.println(taro.getName());
+	}
+
+}
